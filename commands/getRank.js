fbc0473bbff1bbc7d8b76235abb372183f207397
thugipan <thugipan@gmail.com>
2023-01-28 11:18:58 +0100

    Refactor try-catch to get more precise error description

--- commands/getRank.js
+++ commands/getRank.js
@@ -13,8 +13,13 @@ module.exports = {
         try {
             const nameTagLocation = interaction.options.getString('input');
             const splitNameTagLocation = nameTagLocation.split("#");
-            const api = await request(`https://api.henrikdev.xyz/valorant/v1/mmr-history/${splitNameTagLocation[2]}/${splitNameTagLocation[0]}/${splitNameTagLocation[1]}`); // https://api.henrikdev.xyz/valorant/v1/mmr-history/eu/eliif/1312
+            const api = await request(`https://api.henrikdev.xyz/valorant/v1/mmr-history/${splitNameTagLocation[2]}/${splitNameTagLocation[0]}/${splitNameTagLocation[1]}`);
+
+            if (api.statusCode != 200) {
+                throw new SyntaxError('Incorrect Data: Check if you spelled everything correctly!');
+            }
             const jsonResult = await api.body.json();
+
             const parsedResult = new Map([
                 ["Name", jsonResult.name],
                 ["Tag", jsonResult.tag],
@@ -23,6 +28,7 @@ module.exports = {
                 ["RR", jsonResult.data[0].elo],
                 ["Date", jsonResult.data[0].date],
             ]);
+
             await interaction.reply(
                 'Name: ' + parsedResult.get("Name") + '\n' +
                 'Tag: ' + parsedResult.get("Tag") + '\n' +
@@ -31,10 +37,10 @@ module.exports = {
                 'Current RR: ' + parsedResult.get("RR") + '\n' +
                 'Date: ' + parsedResult.get("Date") + '\n'
             );
-        } catch (error) {
-            console.error(error);
-            interaction.reply('There was an error. Try again.');
+        } catch (e) {
+            await interaction.reply({ content: e.message, ephemeral: true });
         }
+
 
     },
 };
