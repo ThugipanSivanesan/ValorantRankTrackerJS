9527d0b734185411901c932fa4b392ed4e22a555
ThugipanSivanesan <thugipan@gmail.com>
2023-03-12 01:23:14 +0100

    Create Logger#

---
+++ src/logger.js
@@ -0,0 +1,22 @@
+const winston = require("winston");
+require("winston-daily-rotate-file");
+
+const dashLog = new winston.transports.DailyRotateFile({
+    filename: "./logs/dash-%DATE%.log",
+    datePattern: "YYYY-MM-DD",
+    zippedArchive: true,
+    maxSize: "20m",
+});
+
+const dash = winston.createLogger({
+    transports: [
+        dashLog,
+        new winston.transports.Console({
+            colorize: true,
+        }),
+    ],
+});
+
+module.exports = {
+    dashLogger: dash,
+};
